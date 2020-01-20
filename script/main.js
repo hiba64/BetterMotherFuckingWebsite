0e410efa230a34f7aca2893b5122605de93239fe
hiba <hibat64@gmail.com>
2020-01-20 19:29:52 +0100

    Changer la couleur des polices des paragraphes et des headings, la police des paragraphes en courier et la police des headers en Comic Sans

--- script/main.js
+++ script/main.js
@@ -16,6 +16,15 @@ window.onload = setTimeout(function(){
 function butpsy() {
   document.getElementById("psy").style.backgroundColor = "magenta";
  
-	
+  document.getElementById("hea").style.fontFamily ="Comic Sans MS";
+var para = document.getElementsByClassName("pg");
+    for (var i = 0; i < para.length; i++) {
+para[i].style.color="blue";
+para[i].style.fontFamily ="Courier New"; 
+    }
+    var hh = document.getElementsByClassName("head");
+    for (var j = 0; j < hh.length; j++) {
+    hh[j].style.color="green"; 
+}
 
 }
